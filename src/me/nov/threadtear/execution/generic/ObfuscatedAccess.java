4150fb21ac6401d80332308d3370728154021600
GraxCode <grax@graxco.de>
2020-04-19 23:38:23 +0200

    don't remove jvm synthetics

--- src/me/nov/threadtear/execution/generic/ObfuscatedAccess.java
+++ src/me/nov/threadtear/execution/generic/ObfuscatedAccess.java
@@ -3,6 +3,9 @@ package me.nov.threadtear.execution.generic;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.MethodNode;
+
 import me.nov.threadtear.asm.Clazz;
 import me.nov.threadtear.asm.util.Access;
 import me.nov.threadtear.execution.Execution;
@@ -12,14 +15,23 @@ import me.nov.threadtear.execution.ExecutionTag;
 public class ObfuscatedAccess extends Execution {
 
 	public ObfuscatedAccess() {
-		super(ExecutionCategory.GENERIC, "Fix obfuscated access", "Fixes obfuscated access like synthetic or bridge.<br>Can break some decompilers, but mostly improves readability", ExecutionTag.POSSIBLE_VERIFY_ERR, ExecutionTag.BETTER_DECOMPILE);
+		super(ExecutionCategory.GENERIC, "Fix obfuscated access", "Fixes obfuscated access like synthetic or bridge.<br>Can break some decompilers, but mostly improves readability",
+				ExecutionTag.POSSIBLE_VERIFY_ERR, ExecutionTag.BETTER_DECOMPILE);
 	}
 
 	@Override
 	public boolean execute(ArrayList<Clazz> classes, boolean verbose) {
-		classes.stream().map(c -> c.node.methods).flatMap(List::stream).forEach(m -> m.access = Access.removeAccess(m.access, ACC_SYNTHETIC, ACC_BRIDGE, ACC_DEPRECATED));
-		classes.stream().map(c -> c.node.fields).flatMap(List::stream).forEach(f -> f.access = Access.removeAccess(f.access, ACC_SYNTHETIC, ACC_BRIDGE, ACC_DEPRECATED));
+		classes.stream().map(c -> c.node.methods).flatMap(List::stream).filter(this::shouldRemove).forEach(m -> m.access = Access.removeAccess(m.access, ACC_SYNTHETIC, ACC_BRIDGE, ACC_DEPRECATED));
+		classes.stream().map(c -> c.node.fields).flatMap(List::stream).filter(this::shouldRemove).forEach(f -> f.access = Access.removeAccess(f.access, ACC_SYNTHETIC, ACC_BRIDGE, ACC_DEPRECATED));
 		logger.info("Removed every synthetic, bridge and deprecated access");
 		return true;
+	}
+
+	public boolean shouldRemove(FieldNode fn) {
+		return !(Access.isFinal(fn.access) && fn.name.matches("(val\\$|this\\$).*"));
+	}
+
+	public boolean shouldRemove(MethodNode mn) {
+		return !(Access.isStatic(mn.access) && mn.name.startsWith("access$"));
 	}
 }
