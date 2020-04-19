34e007114f1980cc16d2958b35dfeac86d4318ac
GraxCode <grax@graxco.de>
2020-04-19 22:58:04 +0200

    check java 7 compatibility first

--- src/me/nov/threadtear/execution/tools/Java7Compatibility.java
+++ src/me/nov/threadtear/execution/tools/Java7Compatibility.java
@@ -1,6 +1,7 @@
 package me.nov.threadtear.execution.tools;
 
 import java.util.ArrayList;
+import java.util.stream.StreamSupport;
 
 import me.nov.threadtear.asm.Clazz;
 import me.nov.threadtear.execution.Execution;
@@ -9,14 +10,25 @@ import me.nov.threadtear.execution.ExecutionTag;
 
 public class Java7Compatibility extends Execution {
 
+	private boolean success;
+
 	public Java7Compatibility() {
-		super(ExecutionCategory.TOOLS, "Make java 7 compatible", "Only works when no lambda expressions and no java 8 specific methods are present.", ExecutionTag.POSSIBLE_DAMAGE);
+		super(ExecutionCategory.TOOLS, "Make Java 7 compatible", "Only works when no lambda expressions and no Java 8 specific methods are present.", ExecutionTag.POSSIBLE_DAMAGE);
 	}
 
 	@Override
 	public boolean execute(ArrayList<Clazz> classes, boolean verbose) {
 		classes.stream().map(c -> c.node).forEach(c -> c.version = 51);
-		logger.info("Compatibility changed to java 7+");
-		return true;
+		success = true;
+		classes.stream().map(c -> c.node).forEach(c -> {
+			c.methods.forEach(m -> {
+				if (StreamSupport.stream(m.instructions.spliterator(), false).anyMatch(ain -> ain.getOpcode() == INVOKEDYNAMIC)) {
+					logger.severe(c.name + "." + m.name + m.desc + " contains instructions that are not supported by Java 7.");
+					success = false;
+				}
+			});
+		});
+		logger.info("Compatibility changed to Java 7+");
+		return success;
 	}
 }
--- src/me/nov/threadtear/execution/tools/Java8Compatibility.java
+++ src/me/nov/threadtear/execution/tools/Java8Compatibility.java
@@ -10,13 +10,14 @@ import me.nov.threadtear.execution.ExecutionTag;
 public class Java8Compatibility extends Execution {
 
 	public Java8Compatibility() {
-		super(ExecutionCategory.TOOLS, "Make java 8 compatible", "Only works when no java 9+ specific methods are present.", ExecutionTag.POSSIBLE_DAMAGE);
+		super(ExecutionCategory.TOOLS, "Make Java 8 compatible", "Only works when no Java 9+ specific methods are present.", ExecutionTag.POSSIBLE_DAMAGE);
 	}
 
 	@Override
 	public boolean execute(ArrayList<Clazz> classes, boolean verbose) {
 		classes.stream().map(c -> c.node).forEach(c -> c.version = 52);
-		logger.info("Compatibility changed to java 8+");
+		//TODO check if bytecode is compatible
+		logger.info("Compatibility changed to Java 8+");
 		return true;
 	}
 }
