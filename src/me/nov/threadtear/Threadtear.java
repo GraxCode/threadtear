8157a16dbce996f384ece6df9622d320a783c10b
GraxCode <grax@graxco.de>
2020-05-23 15:28:55 +0200

    fix size glitch on linux

--- src/me/nov/threadtear/Threadtear.java
+++ src/me/nov/threadtear/Threadtear.java
@@ -104,10 +104,10 @@ public class Threadtear extends JFrame {
   }
 
   private void initBounds() {
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    int width = (int) (screenSize.width * 0.5);
-    int height = (int) (screenSize.height * 0.5);
-    setBounds(screenSize.width / 2 - width / 2, screenSize.height / 2 - height / 2, width, height);
+    DisplayMode displayMode = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode();
+    int width = (int) (displayMode.getWidth() * 0.5);
+    int height = (int) (displayMode.getHeight() * 0.5);
+    setBounds(displayMode.getWidth() / 2 - width / 2, displayMode.getHeight() / 2 - height / 2, width, height);
     setMinimumSize(new Dimension((int) (width / 1.25), (int) (height / 1.25)));
   }
 
