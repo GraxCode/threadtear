9b24316bb2a182178f00dd45e454aac538f7e329
GraxCode <grax@graxco.de>
2020-06-17 22:54:12 +0200

    add javadoc for analyzer interfaces

--- core/src/main/java/me/nov/threadtear/analysis/rewriter/ICRReferenceHandler.java
+++ core/src/main/java/me/nov/threadtear/analysis/rewriter/ICRReferenceHandler.java
@@ -6,6 +6,9 @@ import org.objectweb.asm.tree.analysis.BasicValue;
 
 import me.nov.threadtear.analysis.rewriter.value.CodeReferenceValue;
 
+/**
+ * Same as {@link me.nov.threadtear.analysis.stack.IConstantReferenceHandler} for CodeRewriter
+ */
 public interface ICRReferenceHandler {
 
   Object getFieldValueOrNull(BasicValue v, String owner, String name, String desc);
--- core/src/main/java/me/nov/threadtear/analysis/stack/IConstantReferenceHandler.java
+++ core/src/main/java/me/nov/threadtear/analysis/stack/IConstantReferenceHandler.java
@@ -4,10 +4,32 @@ import java.util.List;
 
 import org.objectweb.asm.tree.analysis.BasicValue;
 
+/**
+ * Use values of referenced fields or methods during stack calculation
+ */
 public interface IConstantReferenceHandler {
 
+  /**
+   * Return a value that is used while calculating stack.
+   *
+   * @param v     Value with the type
+   * @param owner Owner of the field
+   * @param name  Name of the field
+   * @param desc  Desc of the field
+   * @return preferred value or null if unknown
+   */
   Object getFieldValueOrNull(BasicValue v, String owner, String name, String desc);
 
+  /**
+   * Return a value that is used while calculating stack. This is useful if e.g. string length functions are used.
+   *
+   * @param v      Value with the type
+   * @param owner  Owner of the field
+   * @param name   Name of the field
+   * @param desc   Desc of the field
+   * @param values Arguments that are passed, including the object reference
+   * @return preferred value or null if unknown
+   */
   Object getMethodReturnOrNull(BasicValue v, String owner, String name, String desc,
                                List<? extends ConstantValue> values);
 
